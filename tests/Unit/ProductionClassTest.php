58b1778f7863503f3edc9dbe6ec4352935303ee0
Jeroen De Dauw <jeroendedauw@gmail.com>
2019-05-23 11:09:10 +0200

    Best code ever

--- tests/Unit/ProductionClassTest.php
+++ tests/Unit/ProductionClassTest.php
@@ -5,17 +5,94 @@ declare( strict_types = 1 );
 namespace Such\NewProject\Tests\Unit;
 
 use PHPUnit\Framework\TestCase;
-use Such\NewProject\ProductionClass;
 
 /**
- * @covers \Such\NewProject\ProductionClass
- *
  * @licence GNU GPL v2+
  */
 class ProductionClassTest extends TestCase {
 
-	public function testGetTrue() {
-		$this->assertTrue( ProductionClass::getTrue() );
+	public function testParsingOfFullyDeadGrid() {
+		$this->assertEquals(
+			[
+				[ false, false, false ],
+				[ false, false, false ],
+				[ false, false, false ],
+			],
+			$this->parseStringGrid( "...\n...\n..." )
+		);
 	}
+
+	private function parseStringGrid( string $string ) {
+		return array_map(
+			function( string $line ) {
+				return array_map(
+					function( string $character ) {
+						return $character === '*';
+					},
+					str_split( $line )
+				);
+			},
+			explode("\n", $string)
+		);
+	}
+
+	public function testParsingOfFullyAliveGrid() {
+		$this->assertEquals(
+			[
+				[ true, true, true ],
+				[ true, true, true ],
+				[ true, true, true ],
+			],
+			$this->parseStringGrid( "***\n***\n***" )
+		);
+	}
+
+	public function testParsingOfPartiallyAliveGrid() {
+		$this->assertEquals(
+			[
+				[ false, true, false ],
+				[ true, false, true ],
+				[ false, true, false ],
+			],
+			$this->parseStringGrid( ".*.\n*.*\n.*." )
+		);
+	}
+
+	public function testParsingOfDifferentSizeGrid() {
+		$this->assertEquals(
+			[
+				[ false, true ],
+				[ true, false ],
+				[ false, true ],
+			],
+			$this->parseStringGrid( ".*\n*.\n.*" )
+		);
+	}
+
+	public function testCellDiesInCaseOfUnderPopulation0() {
+		$this->assertEquals(
+			false,
+			$this->cellSurvives( true, 0 )
+		);
+	}
+
+	private function cellSurvives( bool $isAlive, int $neighbourCount ): bool {
+		if ( $neighbourCount === 2 || $neighbourCount === 3 ) {
+			return true;
+		}
+
+		return false;
+	}
+
+	public function testCellSurvives() {
+		$this->assertEquals(
+			true,
+			$this->cellSurvives( true, 2 )
+		);
+	}
+
+//	public function finalProductionCodeThing( string $inputString ) {
+//		$this->outputGrid( $this->evolve( $this->parseStringGrid( $inputString ) ) );
+//	}
 
 }
